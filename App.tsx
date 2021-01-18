e1932b75786c823203d6099fa53c90302c5035bc
Trent Larson <trent@trentlarson.com>
2021-01-18 11:29:49 -0700

    successfully finished all setup https://veramo.io/docs/guides/react_native

--- App.tsx
+++ App.tsx
@@ -1,118 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, ScrollView, View, Text, Button } from 'react-native'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+// Import agent from setup
+import { agent } from './src/veramo/setup'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare const global: {HermesInternal: null | {}};
+interface Identifier {
+  did: string
+}
 
 const App = () => {
+  const [identifiers, setIdentifiers] = useState<Identifier[]>([])
+
+  // Add the new identifier to state
+  const createIdentifier = async () => {
+    const _id = await agent.didManagerCreate()
+    setIdentifiers((s) => s.concat([_id]))
+  }
+
+  // Check for existing identifers on load and set them to state
+  useEffect(() => {
+    const getIdentifiers = async () => {
+      const _ids = await agent.didManagerFind()
+      setIdentifiers(_ids)
+
+      // Inspect the id object in your debug tool
+      console.log('_ids:', _ids)
+    }
+
+    getIdentifiers()
+  }, [])
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <SafeAreaView>
+      <ScrollView>
+        <View style={{ padding: 20 }}>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Identifiers</Text>
+          <View style={{ marginBottom: 50, marginTop: 20 }}>
+            {identifiers && identifiers.length > 0 ? (
+              identifiers.map((id: Identifier) => (
+                <View key={id.did}>
+                  <Text>{id.did}</Text>
+                </View>
+              ))
+            ) : (
+              <Text>No identifiers created yet</Text>
+            )}
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          <Button onPress={() => createIdentifier()} title={'Create Identifier'} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default App
---
+++ src/veramo/setup.ts
@@ -0,0 +1,75 @@
+// Core interfaces
+import { createAgent, IDIDManager, IResolver, IDataStore, IKeyManager } from '@veramo/core'
+
+// Core identity manager plugin
+import { DIDManager } from '@veramo/did-manager'
+
+// Ethr did identity provider
+import { EthrDIDProvider } from '@veramo/did-provider-ethr'
+
+// Core key manager plugin
+import { KeyManager } from '@veramo/key-manager'
+
+// Custom key management system for RN
+import { KeyManagementSystem } from '@veramo/kms-local-react-native'
+
+// Custom resolver
+// Custom resolvers
+import { DIDResolverPlugin } from '@veramo/did-resolver'
+import { Resolver } from 'did-resolver'
+import { getResolver as ethrDidResolver } from 'ethr-did-resolver'
+import { getResolver as webDidResolver } from 'web-did-resolver'
+
+// Storage plugin using TypeOrm
+import { Entities, KeyStore, DIDStore, IDataStoreORM } from '@veramo/data-store'
+
+// TypeORM is installed with @veramo/typeorm
+import { createConnection } from 'typeorm'
+
+
+// You will need to get a project ID from infura https://www.infura.io
+const INFURA_PROJECT_ID = '0f439b3b9237480ea8eb9da7b1f3965a'
+
+
+// Create react native db connection
+const dbConnection = createConnection({
+  type: 'react-native',
+  database: 'veramo.sqlite',
+  location: 'default',
+  synchronize: true,
+  logging: ['error', 'info', 'warn'],
+  entities: Entities,
+})
+
+
+export const agent = createAgent<IDIDManager & IKeyManager & IDataStore & IDataStoreORM & IResolver>({
+  plugins: [
+    new KeyManager({
+      store: new KeyStore(dbConnection),
+      kms: {
+        local: new KeyManagementSystem(),
+      },
+    }),
+    new DIDManager({
+      store: new DIDStore(dbConnection),
+      defaultProvider: 'did:ethr:rinkeby',
+      providers: {
+        'did:ethr:rinkeby': new EthrDIDProvider({
+          defaultKms: 'local',
+          network: 'rinkeby',
+          rpcUrl: 'https://rinkeby.infura.io/v3/' + INFURA_PROJECT_ID,
+          gas: 1000001,
+          ttl: 60 * 60 * 24 * 30 * 12 + 1,
+        }),
+      },
+    }),
+    new DIDResolverPlugin({
+      resolver: new Resolver({
+        ethr: ethrDidResolver({
+          networks: [{ name: 'rinkeby', rpcUrl: 'https://rinkeby.infura.io/v3/' + INFURA_PROJECT_ID }],
+        }).ethr,
+        web: webDidResolver().web,
+      }),
+    }),
+  ],
+})
