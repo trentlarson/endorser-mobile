f75e69e4ee51c8fb2c9c3ffeb5e343e122f5bfe4
Trent Larson <trent@trentlarson.com>
2021-01-24 16:50:03 -0700

    send JWT to endorser server

--- src/screens/SignSendToEndorser.tsx
+++ src/screens/SignSendToEndorser.tsx
@@ -1,13 +1,35 @@
+import Debug from 'debug'
 import * as didJwt from 'did-jwt'
+import { DateTime } from 'luxon'
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, ScrollView, View, Text, TextInput, Button } from 'react-native'
+import { ActivityIndicator, Button, Linking, SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native'
 
 import { agent } from '../veramo/setup'
 
+const ENDORSER_API_SERVER = 'http://10.0.0.88:3000'
+const ENDORSER_VIEW_SERVER = 'http://10.0.0.88:3001'
+const debug = Debug('endorser-mobile:share-credential')
+
+function endorserLink(endorserId) {
+  return ENDORSER_VIEW_SERVER + '/reportClaim?claimId=' + endorserId
+}
+
 export function CredentialsScreen({ navigation }) {
   const [claim, setClaim] = useState<string>('')
+  const [endorserId, setEndorserId] = useState<string>(null)
+  const [fetched, setFetched] = useState<boolean>(false)
+  const [fetching, setFetching] = useState<boolean>(false)
   const [identifiers, setIdentifiers] = useState<Identifier[]>([])
   const [jwt, setJwt] = useState<JWT>()
+
+  let currentOrPreviousSat = DateTime.local()
+  if (currentOrPreviousSat.weekday < 6) {
+    // it's not Saturday, so let's default to last Saturday
+    currentOrPreviousSat = currentOrPreviousSat.minus({week:1})
+  }
+  const eventStartDateObj = currentOrPreviousSat.set({weekday:6}).set({hour:9}).startOf("hour")
+  // Hack, but the full ISO pushes the length to 340 which crashes verifyJWT!  Crazy!
+  const TODAY_OR_PREV_START_DATE = eventStartDateObj.toISO({suppressMilliseconds:true})
 
   function bvcClaim(did: string, startTime: string) {
     return {
@@ -37,6 +59,38 @@ export function CredentialsScreen({ navigation }) {
     }
   }
 
+  async function sendToEndorserSite(jwt: string) {
+    setFetching(true)
+    fetch(ENDORSER_API_SERVER + '/api/claim', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Uport-Push-Token': jwt,
+      },
+      body: JSON.stringify({ jwtEncoded: jwt }),
+    })
+    .then(resp => {
+      setFetching(false)
+      setFetched(true)
+      debug('Got endorser.ch status', resp.status)
+      return resp.json()
+    })
+    .then(json => {
+      debug('Got endorser.ch result', json)
+      setEndorserId(json)
+    })
+    .catch(err => {
+      debug('Got error sending to endorser.ch', err)
+      throw Error(
+        'Sorry, got a problem with the endorser.ch response. ' + err,
+      )
+    })
+    .finally(() => {
+      setFetching(false)
+    })
+
+  }
+
   async function signAndSend() {
     /**
     // would like to use https://www.npmjs.com/package/did-jwt-vc
@@ -49,9 +103,10 @@ export function CredentialsScreen({ navigation }) {
     **/
 
     const signer = didJwt.SimpleSigner(identifiers[0].keys[0].privateKeyHex)
-    const did = identifiers[0].did
-    const vcJwt = await didJwt.createJWT(vcPayload(did, claim),{ issuer: did, signer })
+    const did: string = identifiers[0].did
+    const vcJwt: string = await didJwt.createJWT(vcPayload(did, claim),{ issuer: did, signer })
     setJwt(vcJwt)
+    sendToEndorserSite(vcJwt)
   }
 
   // Check for existing identifers on load and set them to state
@@ -59,7 +114,7 @@ export function CredentialsScreen({ navigation }) {
     const getIdentifiers = async () => {
       const _ids = await agent.didManagerFind()
       setIdentifiers(_ids)
-      setClaim(bvcClaim(identifiers[0] ? identifiers[0].did : 'UNKNOWN', '2021-01-24'))
+      setClaim(bvcClaim(identifiers[0] ? identifiers[0].did : 'UNKNOWN', TODAY_OR_PREV_START_DATE))
     }
     getIdentifiers()
   }, [])
@@ -69,12 +124,36 @@ export function CredentialsScreen({ navigation }) {
       <ScrollView>
         <View style={{ padding: 20 }}>
           <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Credentials</Text>
-          <Text>{ identifiers[0] && `ID ${identifiers[0].did}`}
-          </Text>
-          <Button
-            title={'Click to sign & store'}
-            onPress={() => signAndSend()}
-          />
+          <Text>{ identifiers[0] && `ID ${identifiers[0].did}`}</Text>
+          { fetching ? (
+              <View>
+                <Text>Saving to Endorser.ch...</Text>
+                <ActivityIndicator size={'small'} />
+              </View>
+            ) : (
+              fetched ? (
+                endorserId ? (
+                  <Button
+                    title="Success!  Click here to see your claim on Endorser.ch"
+                    onPress={() => {
+                      Linking.openURL(endorserLink(endorserId)).catch(err =>
+                        setError(
+                          'Sorry, something went wrong trying to go to endorser.ch',
+                        ),
+                      )
+                    }}
+                  />
+                ) : (
+                  <Text>Got response from Endorser.ch...</Text>
+                )
+              ) : (
+                <Button
+                  title={'Click to sign & store'}
+                  onPress={() => signAndSend()}
+                />
+              )
+            )
+          }
           { jwt ? (
             <View>
               <Text>JWT</Text>
