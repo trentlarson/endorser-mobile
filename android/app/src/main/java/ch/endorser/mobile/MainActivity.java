d0905ca672dd2f1273979479b4cb2cfc8e7754e5
Trent Larson <trent@trentlarson.com>
2021-03-02 08:10:33 -0700

    immediately after react-native-rename

--- android/app/src/main/java/ch/endorser/mobile/MainActivity.java
+++ android/app/src/main/java/ch/endorser/mobile/MainActivity.java
@@ -1,4 +1,4 @@
-package com.veramo_react_native;
+package ch.endorser.mobile;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Veramo_React_Native";
+    return "EndorserMobile";
   }
 }
