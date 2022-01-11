9c213f59cca4625df8b19dbec507b9dfcb9d53b7
Trent Larson <trent@trentlarson.com>
2022-01-10 20:55:50 -0700

    remove unused test settings

--- src/veramo/appSlice.ts
+++ src/veramo/appSlice.ts
@@ -8,10 +8,6 @@ import * as utility from '../utility/utility'
 
 export const DEFAULT_ENDORSER_API_SERVER = 'https://endorser.ch:3000'
 export const DEFAULT_ENDORSER_VIEW_SERVER = 'https://endorser.ch'
-//const DEFAULT_ENDORSER_API_SERVER = 'http://127.0.0.1:3000'
-//const DEFAULT_ENDORSER_VIEW_SERVER = 'http://127.0.0.1:3001'
-//const DEFAULT_ENDORSER_API_SERVER = 'http://192.168.1.8:3000'
-//const DEFAULT_ENDORSER_VIEW_SERVER = 'http://192.168.1.8:3001'
 
 // for contents set in reducers
 interface Payload<T> {
