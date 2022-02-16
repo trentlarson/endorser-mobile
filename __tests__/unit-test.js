6823039e5eae13a302851228bca39964bb5bab34
Trent Larson <trent@trentlarson.com>
2022-02-15 19:05:07 -0700

    fix tests since we're now relying on 'issuedAt'

--- __tests__/unit-test.js
+++ __tests__/unit-test.js
@@ -30,7 +30,10 @@ test('account Offers & Gives', () => {
   input[0] = { claim: { '@context': 'http://somewhere.else' }}
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input[0] = { claim: { '@context': 'https://schema.org' }}
+  input[0] = {
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: { '@context': 'https://schema.org' },
+  }
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
   input[0].claim['@type'] = 'Offer'
@@ -46,12 +49,15 @@ test('account Offers & Gives', () => {
   input[0].claim.itemOffered = { amountOfThisGood: 'bad' }
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'Offer',
-    itemOffered: { amountOfThisGood: 3 },
-    offeredBy: { identifier: TEST_USER_DID },
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'Offer',
+      itemOffered: { amountOfThisGood: 3 },
+      offeredBy: { identifier: TEST_USER_DID },
+    }
+  })
   outputExp.numUnknowns = 2
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
@@ -63,40 +69,49 @@ test('account Offers & Gives', () => {
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
   const OFFER_ID = '0f21cc1d44412b4ac4cb47973554fd79'
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'Offer',
-    identifier: OFFER_ID,
-    itemOffered: { amountOfThisGood: 2, unitCode: 'HUR' },
-    offeredBy: { identifier: TEST_USER_DID },
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'Offer',
+      identifier: OFFER_ID,
+      itemOffered: { amountOfThisGood: 2, unitCode: 'HUR' },
+      offeredBy: { identifier: TEST_USER_DID },
+    }
+  })
   outputExp.allPromised = [input[1], input[2]]
   outputExp.outstandingCurrencyTotals = { "HUR": 5 }
   outputExp.outstandingInvoiceTotals = { "0f21cc1d44412b4ac4cb47973554fd79": 2 }
   outputExp.totalCurrencyPromised = { "HUR": 5 }
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'Offer',
-    identifier: 'ef56cb471f43cdd024b06baa11a8ce24',
-    itemOffered: { amountOfThisGood: 1, unitCode: 'HUR' },
-    offeredBy: { identifier: TEST_USER_DID },
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'Offer',
+      identifier: 'ef56cb471f43cdd024b06baa11a8ce24',
+      itemOffered: { amountOfThisGood: 1, unitCode: 'HUR' },
+      offeredBy: { identifier: TEST_USER_DID },
+    }
+  })
   outputExp.allPromised = [input[1], input[2], input[3]]
   outputExp.outstandingCurrencyTotals = { "HUR": 6 }
   outputExp.outstandingInvoiceTotals = { "0f21cc1d44412b4ac4cb47973554fd79": 2, "ef56cb471f43cdd024b06baa11a8ce24": 1 }
   outputExp.totalCurrencyPromised = { "HUR": 6 }
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'Offer',
-    itemOffered: { amountOfThisGood: 1, unitCode: 'BTC' },
-    offeredBy: { identifier: TEST_USER_DID },
-    recipient: { identifier: TEST_USER1_DID },
-    validThrough: DateTime.local().plus(Duration.fromISO('P6M')),
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'Offer',
+      itemOffered: { amountOfThisGood: 1, unitCode: 'BTC' },
+      offeredBy: { identifier: TEST_USER_DID },
+      recipient: { identifier: TEST_USER1_DID },
+      validThrough: DateTime.local().plus(Duration.fromISO('P6M')),
+    }
+  })
   outputExp.allPromised = [input[1], input[2], input[3], input[4]]
   outputExp.outstandingCurrencyTotals = { "BTC": 1, "HUR": 6 }
   outputExp.outstandingInvoiceTotals = { "0f21cc1d44412b4ac4cb47973554fd79": 2, "ef56cb471f43cdd024b06baa11a8ce24": 1 }
@@ -104,14 +119,17 @@ test('account Offers & Gives', () => {
   outputExp.totalCurrencyPromised = { "BTC": 1, "HUR": 6 }
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'Offer',
-    itemOffered: { amountOfThisGood: 1, unitCode: 'BTC' },
-    offeredBy: { identifier: TEST_USER_DID },
-    recipient: { identifier: TEST_USER1_DID },
-    validThrough: DateTime.local().minus(Duration.fromISO('P6M')),
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'Offer',
+      itemOffered: { amountOfThisGood: 1, unitCode: 'BTC' },
+      offeredBy: { identifier: TEST_USER_DID },
+      recipient: { identifier: TEST_USER1_DID },
+      validThrough: DateTime.local().minus(Duration.fromISO('P6M')),
+    }
+  })
   outputExp.allPromised = [input[1], input[2], input[3], input[4], input[5]]
   outputExp.totalCurrencyPromised["BTC"] = 2
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
@@ -119,57 +137,72 @@ test('account Offers & Gives', () => {
 
   // Now for "Give"
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'GiveAction',
-    object: { amountOfThisGood: 1, unitCode: 'bad-txn-no-agent' },
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'GiveAction',
+      object: { amountOfThisGood: 1, unitCode: 'bad-txn-no-agent' },
+    }
+  })
   outputExp.numUnknowns = 2
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'GiveAction',
-    agent: { identifier: TEST_USER_DID },
-    object: { amountOfThisGood: 1, unitCode: 'HUR' },
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'GiveAction',
+      agent: { identifier: TEST_USER_DID },
+      object: { amountOfThisGood: 1, unitCode: 'HUR' },
+    }
+  })
   outputExp.totalCurrencyPaid = { "HUR": 1 }
   outputExp.allPaid = [input[7]]
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'GiveAction',
-    agent: { identifier: TEST_USER_DID },
-    offerId: OFFER_ID,
-    object: { amountOfThisGood: 1, unitCode: 'HUR' },
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'GiveAction',
+      agent: { identifier: TEST_USER_DID },
+      offerId: OFFER_ID,
+      object: { amountOfThisGood: 1, unitCode: 'HUR' },
+    }
+  })
   outputExp.totalCurrencyPaid = { "HUR": 2 }
   outputExp.outstandingCurrencyTotals["HUR"] = 5
   outputExp.outstandingInvoiceTotals[OFFER_ID] = 1
   outputExp.allPaid = [input[7], input[8]]
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'GiveAction',
-    agent: { identifier: TEST_USER_DID },
-    object: { amountOfThisGood: 2, unitCode: 'HUR' },
-    recipient: TEST_USER1_DID,
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'GiveAction',
+      agent: { identifier: TEST_USER_DID },
+      object: { amountOfThisGood: 2, unitCode: 'HUR' },
+      recipient: TEST_USER1_DID,
+    }
+  })
   outputExp.totalCurrencyPaid = { "HUR": 4 }
   outputExp.outstandingCurrencyTotals["HUR"] = 5
   outputExp.outstandingInvoiceTotals[OFFER_ID] = 1
   outputExp.allPaid = [input[7], input[8], input[9]]
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
 
-  input = input.concat({ claim: {
-    '@context': 'https://schema.org',
-    '@type': 'GiveAction',
-    agent: { identifier: TEST_USER_DID },
-    object: { amountOfThisGood: 1.5, unitCode: 'BTC' },
-    recipient: 'did:none:test-user-recipient',
-  }})
+  input = input.concat({
+    issuedAt: '2022-02-15 18:58:33Z',
+    claim: {
+      '@context': 'https://schema.org',
+      '@type': 'GiveAction',
+      agent: { identifier: TEST_USER_DID },
+      object: { amountOfThisGood: 1.5, unitCode: 'BTC' },
+      recipient: 'did:none:test-user-recipient',
+    }
+  })
   outputExp.totalCurrencyPaid["BTC"] = 1.5
   outputExp.allPaid = [input[7], input[8], input[9], input[10]]
   expect(utility.countTransactions(input, TEST_USER_DID)).toEqual(outputExp)
