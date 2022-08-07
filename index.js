f01c52d474c033ed11ebe9130604fae8a5c56342
Trent Larson <trent@trentlarson.com>
2022-08-07 16:07:51 -0600

    fix reference to utility for background setup

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import { AppRegistry, Platform } from 'react-native';
 import BackgroundFetch from 'react-native-background-fetch'
 import App from './src/App';
 import { name as appName } from './app.json';
+import * as utility from './src/utility/utility'
 
 AppRegistry.registerComponent(appName, () => App);
 
