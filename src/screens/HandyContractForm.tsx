9c9ad08ca9450d81d39f578129161d7595989d40
Trent Larson <trent@trentlarson.com>
2022-09-05 10:10:21 -0600

    add data to text fields on contract and ability to sign

--- src/screens/HandyContractForm.tsx
+++ src/screens/HandyContractForm.tsx
@@ -1,13 +1,14 @@
 import * as R from 'ramda'
 import React from 'react'
-import { SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native'
+import { Button, SafeAreaView, ScrollView, Text, TextInput, View } from 'react-native'
 
 export function HandyContractFormScreen({ navigation, route }) {
 
   const { onboardingChoice } = route.params
 
-  const title = onboardingChoice.match(/.*?\n/)[0].slice(0, -1)
+  const [data, setData] = React.useState({});
 
+  const title = onboardingChoice.match(/.*?\n/)[0].slice(0, -1)
   const fields = [...onboardingChoice.matchAll(/{{.*?}}/g)].flat()
   const finalFields = R.uniq(fields).map(s => s.slice(2).slice(0, -2))
 
@@ -23,11 +24,16 @@ export function HandyContractFormScreen({ navigation, route }) {
                 <Text>{field.replace('_', ' ')}</Text>
                 <TextInput
                     key={field}
+                    onChangeText={text => setData(R.set(R.lensProp(field), text, data))}
                     style={{ borderWidth: 1, marginTop: 5}}
                   />
               </View>
             )
           }
+          <Button
+            title="Sign"
+            onPress={() => navigation.navigate('Review & Sign', { credentialSubject: { '@type': 'AcceptAction', object: data } })}
+          />
           <View style={{ padding: 20 }} />
           <Text>{ onboardingChoice }</Text>
         </View>
